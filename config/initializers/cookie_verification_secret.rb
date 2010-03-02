536c8a1e2de2733b78432ee5e502ab625a4de8c9
hukl <contact@smyck.org>
2010-03-02 21:54:33 +0100

    initial import

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'cad2b3be89bbcbab22fe2c6fe9a9df9f74b50c1efb7e0f0168cfbb0925e229d3b94186fb4acd66d1b568db9fce3decbe8c898fa0f82381852a00471709b3905a'
